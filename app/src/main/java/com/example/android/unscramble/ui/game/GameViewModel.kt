cf172d7630bd343ecd435dda76fbeb3876ce1722
VoroninaMaria <108742947+VoroninaMaria@users.noreply.github.com>
2023-04-23 15:31:25 +0300

    Attach observer to score and word count

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -7,12 +7,12 @@ import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel(){
 
-    private var _score = 0
-    val score: Int
+    private val _score = MutableLiveData(0)
+    val score: LiveData<Int>
         get() = _score
 
-    private var _currentWordCount = 0
-    val currentWordCount: Int
+    private val _currentWordCount = MutableLiveData(0)
+    val currentWordCount: LiveData<Int>
         get() = _currentWordCount
 
     private var wordsList: MutableList<String> = mutableListOf()
@@ -35,7 +35,7 @@ class GameViewModel : ViewModel(){
             getNextWord()
         } else {
             _currentScrambledWord.value = String(tempWord)
-            ++_currentWordCount
+            _currentWordCount.value = (_currentWordCount.value)?.inc()
             wordsList.add(currentWord)
         }
     }
@@ -46,8 +46,8 @@ class GameViewModel : ViewModel(){
     }
 
     fun reinitializeData() {
-        _score = 0
-        _currentWordCount = 0
+        _score.value = 0
+        _currentWordCount.value = 0
         wordsList.clear()
         getNextWord()
     }
@@ -58,7 +58,7 @@ class GameViewModel : ViewModel(){
     }
 
     private fun increaseScore() {
-        _score += SCORE_INCREASE
+        _score.value = (_score.value)?.plus(SCORE_INCREASE)
     }
 
     fun isUserWordCorrect(playerWord: String): Boolean {
@@ -70,10 +70,12 @@ class GameViewModel : ViewModel(){
     }
 
     fun nextWord(): Boolean {
-        return if (_currentWordCount < MAX_NO_OF_WORDS) {
+        return if (_currentWordCount.value!! < MAX_NO_OF_WORDS) {
             getNextWord()
             true
         } else false
     }
+
+
 
 }
